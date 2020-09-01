f4cef896496f7b3e55da94148ccf7599cd4c24c0
liu Wenxuan <woshitc8535@163.com>
2020-09-01 03:24:26 -0400

    auto import categories

---
+++ src/main/java/com/laioffer/travelplanner/enumerate/CategoryEnum.java
@@ -0,0 +1,16 @@
+package com.laioffer.travelplanner.enumerate;
+
+public enum CategoryEnum {
+    TRANSIT_STATION,
+    BUS_STATION,
+    LODGING,
+    ROUTE,
+    NEIGHBORHOOD,
+    POLITICAL,
+    ZOO,
+    ART_GALLERY,
+    MUSEUM,
+    PARK,
+    TOURIST_ATTRACTION,
+    UNIVERSITY
+}
