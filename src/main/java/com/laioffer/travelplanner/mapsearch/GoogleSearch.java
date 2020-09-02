2ee33bc37fbd12ff228fc47ddbf933f78361defe
liu Wenxuan <woshitc8535@163.com>
2020-09-01 23:03:39 -0400

    Add get all places

--- src/main/java/com/laioffer/travelplanner/mapsearch/GoogleSearch.java
+++ src/main/java/com/laioffer/travelplanner/mapsearch/GoogleSearch.java
@@ -29,64 +29,6 @@ public class GoogleSearch {
 
 
     public static void main(String[] args) {
-        GooglePlaces client = new GooglePlaces("AIzaSyBFNRmLV8bEPw15tE1fN79y-rlhTmw5Lso");
-        List<Place> places = client.getPlacesByQuery("Brooklyn Bridge", GooglePlaces.MAXIMUM_RESULTS);
-
-        GeoApiContext context = new GeoApiContext.Builder()
-                .apiKey("AIzaSyBFNRmLV8bEPw15tE1fN79y-rlhTmw5Lso")
-                .build();
-
-        PlaceDetailsRequest request = PlacesApi.placeDetails(context, places.get(0).getPlaceId());
-        //request.fields(FieldMask.)
-        try {
-            PlaceDetails details = request.await();
-            for (int i = 0; i < details.photos.length; i++) {
-                System.out.println(details.photos[i].htmlAttributions[0]);
-                System.out.println(details.photos[i].photoReference);
-
-            }
-//            System.out.println(details.rating);
-//            System.out.println(details.name);
-//            System.out.println(details.placeId);
-//            System.out.println(details.formattedAddress);
-//            System.out.println(details.adrAddress);
-//            System.out.println(details.url);
-//            System.out.println(details.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-
-
-        List<Place> places = client.getPlacesByQuery("Empire State Building", GooglePlaces.MAXIMUM_RESULTS);
-
-        Place empireStateBuilding = null;
-        for (Place place : places) {
-            if (place.getName().equals("Times Square")) {
-                empireStateBuilding = place;
-                break;
-            }
-        }
-
-        if (empireStateBuilding != null) { ; // sends a GET request for more details
-            // Just an example of the amount of information at your disposal:
-            System.out.println("Name: " + empireStateBuilding.getName());
-            System.out.println("Phone: " + empireStateBuilding.getPhoneNumber());
-            System.out.println("International Phone: " + empireStateBuilding.getInternationalPhoneNumber());
-            System.out.println("Website: " + empireStateBuilding.getWebsite());
-            System.out.println("Status: " + empireStateBuilding.getStatus());
-            System.out.println("Google Place URL: " + empireStateBuilding.getGoogleUrl());
-            System.out.println("Price: " + empireStateBuilding.getPrice());
-            System.out.println("Address: " + empireStateBuilding.getAddress());
-            System.out.println("Vicinity: " + empireStateBuilding.getVicinity());
-            System.out.println("Reviews: " + empireStateBuilding.getReviews().size());
-            System.out.println("Hours:\n " + empireStateBuilding.getHours());
-        }
-
-        System.out.println(places.get(0).getPlaceId());
-        System.out.println(places.toString());
-
 //        Place place = client.getPlaceById(places.get(0).getPlaceId());
 //        System.out.println(place.toString());
 //        Place empireStateBuilding = null;
--- src/main/java/com/laioffer/travelplanner/repositories/PlaceRepository.java
+++ src/main/java/com/laioffer/travelplanner/repositories/PlaceRepository.java
@@ -3,12 +3,11 @@ package com.laioffer.travelplanner.repositories;
 import com.laioffer.travelplanner.entities.Place;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlaceRepository extends ElasticsearchRepository<Place, String> {
-	Iterable<Place> findAll();
-
-
+	List<Place> findAllByPlaceNameStartsWith(String name);
 	Optional<Place> findByPlaceName(String name);
 	Optional<Place> findByPlaceId(String placeId);
     
